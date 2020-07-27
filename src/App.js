1ffcc5622924570c24e5cd3637dc665aa88241a4
Leonardo <leonardo@casbrasil.com.br>
2020-07-27 15:18:40 -0300

    Update json. Customize content of the site.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription={"Você ja quis saber como se iniciou o universo de warcraft e hearthstone? venha aprender mais sobre a Lore do vasto universo de warcraft com @cojanowow e @AlexCDuran!"}
       />
 
       <Carousel
@@ -27,20 +27,7 @@ function App() {
 
       <Carousel
         category={dadosIniciais.categorias[2]}
-      />      
-
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />      
-
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      />      
-
-      <Carousel
-        category={dadosIniciais.categorias[5]}
-      />      
-
+      />     
       <Footer />
     </div>
   );
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,14 +4,20 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
       <p>
         Orgulhosamente criado durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
+        </a>
+      </p>
+      <p>
+        Desenvolvido por
+        {' '}
+        <a href="https://www.linkedin.com/in/leonardocarvalho133/">
+          Leonardo Carvalho
+        </a>
+        <a class="fa fa-github" href="https://www.linkedin.com/in/leonardocarvalho133/">
         </a>
       </p>
     </FooterBase>
